ec33f31998b465a8e41cfaf21c9cfe6d51ab8207
mrngocthien <108567256+mrngocthien@users.noreply.github.com>
2023-01-02 10:31:47 +0700

    up

--- caseStudy/snakeGame/app.js
+++ caseStudy/snakeGame/app.js
@@ -109,9 +109,10 @@ function getScore() {
 }
 function getTimer() {
     var sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; };
+    function pad (val) { return val > 9 ? val : "0" + val; };
     setInterval(() => {
-        document.getElementById("seconds").innerHTML=pad(++sec%60);
-        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+        document.getElementById("seconds").innerHTML = pad(++sec % 60);
+        document.getElementById("minutes").innerHTML = pad(parseInt(sec/60, 10));
+        document.getElementById("hours").innerHTML = pad(parseInt(sec/360, 10));
     }, 1000);
 }
